1838bb22098cdf03f59eb7477d15bbd408c26136
kacavendadino <kacavendadino@gmail.com>
2018-11-04 12:35:44 +0100

    popup na entry

--- EmployeeVacationCalendar.FE/src/components/calendar/calendarEntry.tsx
+++ EmployeeVacationCalendar.FE/src/components/calendar/calendarEntry.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { RoutesEnum, EmployeeTypeEnum } from '../../common/enums';
 import { ICalendarEntry, IUserInfo } from '../../common/data';
-import { Label, Icon } from 'semantic-ui-react';
+import { Label, Icon, Popup } from 'semantic-ui-react';
 import { getVacationTypeResources, getVacationTypeClassName } from '../../common/vacationType';
+import { Moment } from 'moment';
+import moment = require('moment');
 
 export interface ICalendarEntryProps {
     loggedUserInfo?: IUserInfo;
@@ -23,11 +25,11 @@ export default class CalendarEntry extends React.Component<ICalendarEntryProps> 
             (loggedUserInfo.type !== EmployeeTypeEnum.User
                 || loggedUserInfo.type === EmployeeTypeEnum.User && loggedUserInfo.id === entry.employeeId);
 
+        const { text } = getVacationTypeResources(entry.vacationType);
+        const calendarEntry = isEditable ? <Link to={`${RoutesEnum.NewCalendarEntry}/${entry.id}`}>{this._renderEntry()}</Link>
+            : this._renderEntry();
         return <div className="calendar-entry">
-            {isEditable && <Link to={`${RoutesEnum.NewCalendarEntry}/${entry.id}`}>
-                {this._renderEntry()}
-            </Link>}
-            {!isEditable && this._renderEntry()}
+            <Popup trigger={calendarEntry} header={text} content={this._getDateRangeText(entry.startDate, entry.endDate)} />
         </div>;
     }
 
@@ -36,5 +38,9 @@ export default class CalendarEntry extends React.Component<ICalendarEntryProps> 
         const { icon } = getVacationTypeResources(entry.vacationType);
         const className = getVacationTypeClassName(entry.vacationType);
         return <Label className={className}><Icon name={icon as any} />{`${employee.lastName}, ${employee.firstName}`}</Label>;
+    }
+
+    private _getDateRangeText = (startDate: Date, endDate: Date) => {
+        return `${moment(startDate).format("DD-MM-YYYY")} - ${moment(endDate).format("DD-MM-YYYY")}`;
     }
 }
